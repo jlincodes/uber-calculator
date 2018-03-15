a01a60eee58f023c92f903876c302faf5257480a
Julie <julie.lin0812@gmail.com>
2018-03-15 16:42:08 -0700

    Gets calculator working and adds "clear" button

--- src/App.js
+++ src/App.js
@@ -7,58 +7,115 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currValues: [],
+      currValues: "",
       operation: "",
+      result: ""
     };
     this.storeValues = this.storeValues.bind(this);
-    this.storeOperation = this.storeOperation.bind(this);
     this.handleCalc = this.handleCalc.bind(this);
+    this.clear = this.clear.bind(this);
   }
 
   storeValues(event) {
     event.preventDefault();
-    const newValues = this.state.currValues;
-    newValues.push(event.target.innerText);
+    let newValues = this.state.currValues;
+    newValues = newValues + event.target.innerText;
     this.setState({ currValues: newValues });
-    // console.log(this.state);
-  }
-
-  storeOperation(event) {
-    event.preventDefault();
-
   }
 
   handleCalc(event) {
     event.preventDefault();
+    const operators = "+-*/";
+    const str = (this.state.currValues);
+    const arr = str.split('');
+    let operator;
+    let newResult;
 
+    arr.forEach( el => {
+      if (operators.includes(el)) {
+        operator = el;
+      }
+    });
+
+    let values = str.split(operator);
+
+    console.log(values);
+
+    let val1 = parseInt(values[0]);
+    let val2 = parseInt(values[1]);
+
+    switch (operator) {
+
+      case ("+"):
+        newResult = val1 + val2;
+        break;
+      case ("-"):
+        newResult = val1 - val2;
+        break;
+      case ("*"):
+        newResult = val1 * val2;
+        break;
+      case ("/"):
+        newResult = val1 / val2;
+        break;
+      default:
+        break;
+    }
+
+    newResult = newResult.toString();
+    this.setState({
+      currValues: newResult,
+      operation: "",
+      result: newResult
+    });
+
+    console.log("state", this.state);
+  }
+
+  clear(event) {
+    event.preventDefault();
+    this.setState({
+      currValues: "",
+      operation: "",
+      result: ""
+    });
   }
 
   render() {
+    let result = 0;
+    if (this.state.result) {
+      result = this.state.result;
+    } else if (this.state.currValues) {
+      result = this.state.currValues;
+    }
     return (
       <div className="App">
-        <input type="text" disabled></input>
+        <div className="results">
+          <span>{result}</span>
+        </div>
         <div>
           <button onClick={this.storeValues} className="num-btn">7</button>
           <button onClick={this.storeValues} className="num-btn">8</button>
           <button onClick={this.storeValues} className="num-btn">9</button>
-          <button onClick={this.storeOperation} className="operator-btn">/</button>
+          <button onClick={this.storeValues} className="operator-btn">/</button>
         </div>
         <div>
           <button onClick={this.storeValues} className="num-btn">4</button>
           <button onClick={this.storeValues} className="num-btn">5</button>
           <button onClick={this.storeValues} className="num-btn">6</button>
-          <button onClick={this.storeOperation}className="operator-btn">*</button>
+          <button onClick={this.storeValues} className="operator-btn">*</button>
         </div>
         <div>
           <button onClick={this.storeValues} className="num-btn">1</button>
           <button onClick={this.storeValues} className="num-btn">2</button>
           <button onClick={this.storeValues} className="num-btn">3</button>
-          <button onClick={this.storeOperation}className="operator-btn">-</button>
+          <button onClick={this.storeValues} className="operator-btn">-</button>
         </div>
         <div>
+          <button onClick={this.clear} className="clear-btn">C</button>
           <button onClick={this.storeValues} className="num-btn">0</button>
-          <button onClick={this.storeOperation}className="operator-btn">+</button>
           <button onClick={this.handleCalc} className="operator-btn">=</button>
+          <button onClick={this.storeValues}className="operator-btn">+</button>
         </div>
 
       </div>
