5e84c2c562319cb96106d13d175bcc41f79ab705
Julie <julie.lin0812@gmail.com>
2018-03-15 14:25:46 -0700

    Adds basic UI layout

--- src/App.js
+++ src/App.js
@@ -6,7 +6,31 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <input type="text" disabled></input>
+        <div>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>/</button>
+        </div>
+        <div>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>*</button>
+        </div>
+        <div>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>-</button>
+        </div>
+        <div>
+          <button>0</button>
+          <button>+</button>
+          <button>=</button>
+        </div>
+
       </div>
     );
   }
